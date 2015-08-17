676e171f9344a5367de34fa4605e36b0e801427b
Armando Galván <zareek99@gmail.com>
2015-08-17 17:05:58 -0500

    Initial Commit

---
+++ Tarea2RangePrimes/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tarea2RangePrimes
+//
+//  Created by Zareek Galvan on 8/17/15.
+//  Copyright (c) 2015 Zareek Galvan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
