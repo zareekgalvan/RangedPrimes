d374d927f046503a718f8fb5cb1644e143c2f799
Zareek Galván <zareek99@gmail.com>
2015-10-17 18:34:23 -0500

    Gives you the ranged primes

--- Tarea2RangePrimes/main.cpp
+++ Tarea2RangePrimes/main.cpp
@@ -7,9 +7,83 @@
 //
 
 #include <iostream>
+#include <math.h>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+//=======================================================================================================================
+bool isPrime(int intNumber)
+{
+    if (intNumber == 2 || intNumber == 3)
+    {
+        return true;
+    }
+    
+    if (intNumber % 2 == 0 && intNumber != 2)
+    {
+        return false;
+    }
+    
+    if (intNumber % 3 == 0 && intNumber != 3)
+    {
+        return false;
+    }
+    
+    for (int i = 3; i*i <= intNumber; i += 2)
+    {
+        if (intNumber % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+//=======================================================================================================================
+int subPrevPrime(int intNumber)
+{
+    if (isPrime(intNumber))
+    {
+        return intNumber;
+    }
+    
+    int intPrevPrime = intNumber - 1;
+    
+    while (!isPrime(intPrevPrime))
+    {
+        intPrevPrime -= 1;
+    }
+    
+    return intPrevPrime;
+}
+
+//=======================================================================================================================
+int subNextPrime(int intNumber)
+{
+    if (isPrime(intNumber))
+    {
+        return intNumber;
+    }
+    
+    int intNextPrime = intNumber + 1;
+    
+    while (!isPrime(intNextPrime))
+    {
+        intNextPrime += 1;
+    }
+    
+    return intNextPrime;
+}
+
+//=======================================================================================================================
+int main()
+{
+    int intTimes;
+    cin >> intTimes;
+    
+    for (int intI = 0; intI < intTimes; intI++)
+    {
+        int intNumber;
+        cin >> intNumber;
+        cout << subPrevPrime(intNumber) << " " << subNextPrime(intNumber) << endl;
+    }
 }
